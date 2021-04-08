3e073ffe6cb83f2cdd6eee56b0c6d25697d7605f
Antoine42ASSE <81389113+Antoine42ASSE@users.noreply.github.com>
2021-04-08 11:22:26 +0200

    Add files via upload

---
+++ Etudient 1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cmarh>
+#include "TD_GitHub.cpp"
+
+//Fonctions réalisées par l'étudiant 1 :
+// Majeur :
+
+bool estMajeur (int age )
+{
+  int age;
+  bool majeure
+  cout <<"Entrer votre age" << endl;
+  cin >> age;
+  
+  if (age>= 18)
+  {
+	  majeur=true
+	  return vrai
+  }
+  
+  else
+  {
+	  majeur=false
+	  return faux 
+  }
+  
+  
+  // Exposant
+  
+  int exposant(int nbr, int exp)
+  {
+  int resultat,nbr,exp;
+  
+  cout << "Entrer un nombre s'il vous plait" << endl;
+  cin >> nbr;
+  cout << "Entrer son exposant" << endl;
+  cin >> exp;
+  
+  resultat=(pow(nbr,exp))
+  return resultat;
+  }
+  
+  // TVA
+  
+  float tva(int prix)
+  {
+	  int prix; 
+	  float resultat;
+	  
+	  cout << Veuillez entrer le prix s'il vous plait" << endl;
+	  cin >> prix;
+	  
+	  resultat= (prix/100)*19,6.
+	  return resultat;
+	  
+  }
+  
+  // Plus grand
+  
+  int Plusgrand (int a, int b, int c)
+  {
+	  int nb1,nb2,nb3;
+	  
+	  cout << "Veuillez entrer le premier nombre" << endl;
+	  cin >> nb1;
+	  cout << "Veuillez entrer le deuxieme nombre" << endl;
+	  cin >> nb2;
+	  cout << "Veuillez entrer le troisieme nombre" << endl;
+	  cin >> nb3;
+	  
+	  if
+	  { nb1<nb2 and nb1<nb3
+	  return nb1
+	  }
+	  
+	  else if 
+	  { nb2<nb1 and nb2<nb3
+	  return nb2
+	  }
+	  
+	  else return nb3
+  }
+	 
+	  
